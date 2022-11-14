2290966ad7c5c1a70e2f2fbf8721f7a978cc61aa
Santiago <wupengfa007@gmail.com>
2022-11-14 14:24:58 +0800

    feat: change template

---
+++ project/custom-tab-bar/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    icon: 'home',
+    text: '聊天',
+    url: 'pages/home/home',
+  },
+  {
+    icon: 'sort',
+    text: '通讯录',
+    url: 'pages/goods/category/index',
+  },
+  {
+    icon: 'cart',
+    text: '资讯',
+    url: 'pages/cart/index',
+  },
+  {
+    icon: 'person',
+    text: '我的',
+    url: 'pages/usercenter/index',
+  },
+];
